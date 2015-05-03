59a1fb57a2fab61e9a18bc72135201978193e10a
whitequark <whitequark@whitequark.org>
2015-05-03 23:00:02 +0300

    Update ast.shim for Python 3.

--- pyparser/shim/ast.py
+++ pyparser/shim/ast.py
@@ -16,3 +16,16 @@ if sys.version_info <= (2, 6):
 
     class SetComp(expr):
         _fields = ('elt', 'generators')
+
+if sys.version_info >= (3,):
+    class Repr(expr):
+        _fields = ('value',)
+
+    class Exec(expr):
+        _fields = ('body', 'globals', 'locals')
+
+    class Print(expr):
+        _fields = ('dest', 'values', 'nl')
+
+    class TryExcept(stmt):
+        _fields = ('body', 'handlers', 'orelse')

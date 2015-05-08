3650f5c348c0e4fcee383d7a8dde527b51ce6aa4
whitequark <whitequark@whitequark.org>
2015-05-08 10:40:43 +0300

    More consistent documentation.

--- pyparser/__init__.py
+++ pyparser/__init__.py
@@ -7,18 +7,19 @@ def parse(source, filename='<unknown>', mode='exec',
     Parse a string into an abstract syntax tree.
     This is the replacement for the built-in :meth:`..ast.parse`.
 
-    :param str source: Source code in the correct encoding
-    :param filename: Filename of the source (used in diagnostics)
-    :param mode: Execution mode. Pass ``"exec"`` to parse a module,
+    :param source: (string) Source code in the correct encoding
+    :param filename: (string) Filename of the source (used in diagnostics)
+    :param mode: (string) Execution mode. Pass ``"exec"`` to parse a module,
         ``"single"`` to parse a single (interactive) statement,
         and ``"eval"`` to parse an expression. In the last two cases,
         ``source`` must be terminated with an empty line
         (i.e. end with ``"\\n\\n"``).
-    :param flags: Future flags. Equivalent to ``from __future__ import <flags>``.
-    :param int,int version: A tuple of the major and minor version
-        of Python syntax to recognize.
-    :return ast.AST: abstract syntax tree
-    :raise diagnostic.DiagnosticException:
+    :param flags: (list of string) Future flags.
+        Equivalent to ``from __future__ import <flags>``.
+    :param version: (2-tuple of int) Major and minor version of Python
+        syntax to recognize.
+    :return: (:class:`ast.AST`) abstract syntax tree
+    :raise: :class:`diagnostic.DiagnosticException`
         if the source code is not well-formed
     """
     buffer = pyparser.source.Buffer(source, filename)

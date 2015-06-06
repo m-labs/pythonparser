2167c885d73b93f7cf44a59d4704181141bd26f2
whitequark <whitequark@whitequark.org>
2015-06-06 17:43:36 +0300

    algorithm.{Visitor,Transformer}: don't die on lists of strings.

--- pythonparser/algorithm.py
+++ pythonparser/algorithm.py
@@ -37,7 +37,7 @@ class Visitor:
         """Visit a node or a list of nodes. Other values are ignored"""
         if isinstance(obj, list):
             for elt in obj:
-                self._visit_one(elt)
+                self.visit(elt)
         elif isinstance(obj, ast.AST):
             self._visit_one(obj)
 
@@ -79,7 +79,7 @@ class Transformer:
     def visit(self, obj):
         """Visit a node or a list of nodes. Other values are ignored"""
         if isinstance(obj, list):
-            return list(filter(lambda x: x is not None, map(self._visit_one, obj)))
+            return list(filter(lambda x: x is not None, map(self.visit, obj)))
         elif isinstance(obj, ast.AST):
             return self._visit_one(obj)
 

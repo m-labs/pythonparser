a21205af3858c672a630363d9744adeadd9269a6
whitequark <whitequark@whitequark.org>
2015-06-06 17:27:13 +0300

    Add forgotten import.

--- pythonparser/diagnostic.py
+++ pythonparser/diagnostic.py
@@ -5,6 +5,7 @@ and presentation of diagnostic messages.
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 from functools import reduce
+import sys
 
 class Diagnostic:
     """
